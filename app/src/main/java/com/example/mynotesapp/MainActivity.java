8998c13b21577e67167527327c672e9e86799c80
Dineth Jayawickrama <dineth2226@gmail.com>
2024-06-28 20:59:46 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/mynotesapp/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.mynotesapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mloginemail,mloginpassword;
+    private RelativeLayout mlogin,mgotosignup;
+    private TextView mgotoforgotpassword;
+
+    private FirebaseAuth firebaseAuth;
+
+    ProgressBar mprogressbarofmainactivity;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar();
+
+        mloginemail=findViewById(R.id.loginemail);
+        mloginpassword=findViewById(R.id.loginpassword);
+        mlogin=findViewById(R.id.login);
+        mgotoforgotpassword=findViewById(R.id.gotoforgotpassword);
+        mgotosignup=findViewById(R.id.gotosignup);
+        mprogressbarofmainactivity=findViewById(R.id.progressbarofmainactivity);
+
+        firebaseAuth=firebaseAuth.getInstance();
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+
+        if(firebaseUser!=null)
+        {
+            finish();
+            startActivity(new Intent(MainActivity.this, notesactivity.class));
+        }
+
+
+        mgotosignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUp.class));
+            }
+        });
+
+        mgotoforgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,frogotpassword.class));
+            }
+        });
+
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail=mloginemail.getText().toString().trim();
+                String password=mloginpassword.getText().toString().trim();
+
+                if(mail.isEmpty()|| password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    //login user
+
+                    mprogressbarofmainactivity.setVisibility(View.VISIBLE);
+
+                    firebaseAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful())
+                            {
+                              checkmailverfication();
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"Account Doesn't Exist",Toast.LENGTH_SHORT).show();
+                                mprogressbarofmainactivity.setVisibility(View.INVISIBLE);
+                            }
+
+                        }
+                    });
+
+                }
+            }
+        });
+
+
+    }
+
+    private void checkmailverfication()
+    {
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+
+        if(firebaseUser.isEmailVerified()==true)
+        {
+            Toast.makeText(getApplicationContext(),"Logged In",Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(new Intent(MainActivity.this,notesactivity.class));
+        }
+        else
+        {
+            mprogressbarofmainactivity.setVisibility(View.INVISIBLE);
+            Toast.makeText(getApplicationContext(),"Verify your mail first",Toast.LENGTH_SHORT).show();
+            firebaseAuth.signOut();
+        }
+    }
+
+
+}
